54884e73b3740b221134892ddddbb5fc30e5991e
Priyanshu Paul <priyanshupaul90@gmail.com>
2025-08-05 22:27:06 +0530

    Binary Search Questions (Pt1)

---
+++ Searching/Binary Search/Floor_of_a_number.java
@@ -0,0 +1,37 @@
+public class Floor_of_a_number {
+
+     public static void main(String[] args) {
+
+        int[]arr = {2 ,3, 5, 9 ,14, 16, 18};
+        int target = 15;
+
+        int ans = floorOfANumber(arr, target);
+
+        System.out.println(ans);
+        
+    }
+
+    static int floorOfANumber(int[]arr, int target){
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start<=end){
+            int mid = start + (end - start);
+
+            if(target < arr[mid]){
+                end = mid - 1;
+            }
+
+            else if(target > arr[mid]){
+                start = mid + 1;
+            }
+
+            else{
+                return mid;
+            }
+        }
+
+        return end;
+    }
+}
